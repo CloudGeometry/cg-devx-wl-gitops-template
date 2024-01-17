b0a4ecaf0702d8597adb5ad2f544e6e893c09f1c
Sergei Zhekpisov <szhekpisov@upteam.com>
2024-01-17 17:04:45 +0000

    add sns

--- terraform/infrastructure/samples/aws/sns.tf
+++ terraform/infrastructure/samples/aws/sns.tf
@@ -1,3 +1,6 @@
+# AWS S3 bucket with versioning
+# For more details please see official provider documentation https://registry.terraform.io/modules/terraform-aws-modules/sns/aws/latest
+
 module "sns_topic" {
   source  = "terraform-aws-modules/sns/aws"
 
